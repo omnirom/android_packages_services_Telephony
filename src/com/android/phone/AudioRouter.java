c4aa002e9584139baa79aa17450f946241a482f1
h3llrais3r <pooh_beer_1@hotmail.com>
2014-03-19 21:53:08 +0100

    Ported my low-incall volume fix from CM-10.2 to CM-11.0
    Change-Id: I5eb276744754f7d728b2e6ecf2c65a836f51c422

--- src/com/android/phone/AudioRouter.java
+++ src/com/android/phone/AudioRouter.java
@@ -179,7 +179,12 @@ import java.util.List;
 
     public void onMuteChange(boolean muted) {
         logD("onMuteChange: " + muted);
+        
         notifyListeners();
+        
+        // Fix for low in-call volume bug
+        // Reset the audio volume stream after mute change
+        PhoneUtils.resetAudioStreamVolume();
     }
 
     /**
@@ -350,6 +355,10 @@ import java.util.List;
         if (doNotify) {
             notifyListeners();
         }
+        
+        // Fix for low in-call volume bug
+        // Reset the audio volume stream after switching audio mode
+        PhoneUtils.resetAudioStreamVolume();
     }
 
     /**
