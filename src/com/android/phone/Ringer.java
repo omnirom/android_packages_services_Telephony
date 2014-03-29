c7c7f692b8961bfca98a5d4c3b8efeae8005e303
Lars Greiss <kufikugel@googlemail.com>
2014-03-29 17:10:22 +0700

    [2/3] Telephony: update Quiet Hours
    Quiet Hours: Allow disabling phone ringer
    https://github.com/CyanogenMod/android_packages_services_Telephony/commit/be3d06646571fa83600c0dffbad17661a0f12ee1
    
    Change-Id: Ie776d46e296a9448fbb3a38f5c5de3f7565fed67

--- src/com/android/phone/Ringer.java
+++ src/com/android/phone/Ringer.java
@@ -35,7 +35,6 @@ import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.Log;
 
-import com.android.internal.util.slim.QuietHoursHelper;
 import com.android.internal.telephony.Phone;
 /**
  * Ringer manager for the Phone app.
@@ -176,7 +175,7 @@ public class Ringer {
                 mVibratorThread.start();
             }
             int ringerVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_RING);
-            if (ringerVolume == 0 && mRingerVolumeSetting <= 0 || QuietHoursHelper.inQuietHours(mContext, Settings.System.QUIET_HOURS_RINGER)) {
+            if (ringerVolume == 0 && mRingerVolumeSetting <= 0) {
                 if (DBG) log("skipping ring because volume is zero");
                 return;
             }
