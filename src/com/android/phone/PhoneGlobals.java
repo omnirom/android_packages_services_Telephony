28110536e2bb3811c2a20e5d5544fc96a2c28e20
Nazish Tabassum <ntabassu@codeaurora.org>
2020-04-10 18:37:47 +0530

    1.5 HAL version check support for sim-deperso
    If HAL Version is 1.5 or above, then functionality uses
    IRadio interface to support sim-deperso feature
    otherwise fallback to follow proprietary legacy interface.
    
    Change-Id: I70279bf25ec608b62854540363583e89823e4d1a
    CRs-Fixed: 2672590

--- src/com/android/phone/PhoneGlobals.java
+++ src/com/android/phone/PhoneGlobals.java
@@ -114,7 +114,6 @@ public class PhoneGlobals extends ContextWrapper {
     // Message codes; see mHandler below.
     private static final int EVENT_SIM_NETWORK_LOCKED = 3;
     private static final int EVENT_SIM_STATE_CHANGED = 8;
-    private static final int EVENT_SIM_STATE_CHANGED_CHECKREADY = 18;
     private static final int EVENT_DATA_ROAMING_DISCONNECTED = 10;
     private static final int EVENT_DATA_ROAMING_CONNECTED = 11;
     private static final int EVENT_DATA_ROAMING_OK = 12;
@@ -242,15 +241,6 @@ public class PhoneGlobals extends ContextWrapper {
                         IccNetworkDepersonalizationPanel.showDialog(phone, subType);
                     }
                     break;
-
-                case EVENT_SIM_STATE_CHANGED_CHECKREADY:
-                    if (msg.obj.equals(IccCardConstants.INTENT_VALUE_ICC_READY) ||
-                            msg.obj.equals(IccCardConstants.INTENT_VALUE_ICC_LOADED)) {
-                        Log.i(LOG_TAG, "Dismissing depersonal panel");
-                        IccNetworkDepersonalizationPanel.dialogDismiss(0);
-                    }
-                    break;
-
                 case EVENT_DATA_ROAMING_DISCONNECTED:
                     notificationMgr.showDataRoamingNotification(msg.arg1, false);
                     break;
@@ -694,8 +684,8 @@ public class PhoneGlobals extends ContextWrapper {
                     PhoneUtils.registerIccStatus(mHandler, EVENT_SIM_NETWORK_LOCKED, phoneId);
                 }
                 String iccStatus = intent.getStringExtra(IccCardConstants.INTENT_KEY_ICC_STATE);
-                mHandler.sendMessage(mHandler.obtainMessage(EVENT_SIM_STATE_CHANGED_CHECKREADY,
-                        iccStatus));
+                mHandler.sendMessage(mHandler.obtainMessage(EVENT_SIM_STATE_CHANGED,
+                        new EventSimStateChangedBag(phoneId, iccStatus)));
                 Phone phone = PhoneFactory.getPhone(phoneId);
                 if (phone != null) {
                     if (IccCardConstants.INTENT_VALUE_ICC_LOADED.equals(simStatus)) {
